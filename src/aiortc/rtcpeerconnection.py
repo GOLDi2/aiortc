c44bde578257d9e414bfede34f5149668b89b4d0
Johannes Nau <johannes.nau@tu-ilmenau.de>
2022-11-20 14:44:21 +0100

    emit RTCTrackEvent directly

--- src/aiortc/rtcpeerconnection.py
+++ src/aiortc/rtcpeerconnection.py
@@ -979,7 +979,7 @@ class RTCPeerConnection(AsyncIOEventEmitter):
 
         # FIXME: in aiortc 2.0.0 emit RTCTrackEvent directly
         for event in trackEvents:
-            self.emit("track", event.track)
+            self.emit("track", event)
 
         # connect
         asyncio.ensure_future(self.__connect())
